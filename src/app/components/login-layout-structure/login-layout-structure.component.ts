2f836cb32d747cb07ab409a82aa26b0ffc565b2c
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-08-04 13:07:23 -0300

    feat: add disabled value to submit button

--- src/app/components/login-layout-structure/login-layout-structure.component.ts
+++ src/app/components/login-layout-structure/login-layout-structure.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class LoginLayoutStructureComponent {
   @Input() title: string = '';
+  @Input() disabledBtn: boolean = true;
   @Output('submit') onSubmit = new EventEmitter();
 
   submit() {
