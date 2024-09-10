b7ef7d9f4882c3a0b4ca8c9a3752cc1dee52400b
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-09-10 19:48:19 -0300

    feat: route home added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { MainComponent } from './components/shared/main/main.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
@@ -17,6 +19,16 @@ const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent,
+  },
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+    ],
   },
 ];
 
